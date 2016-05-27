e1eeff2882f1dcd71bc23802b5409b4815a8aa22
KristenKarcher <kristenmkarcher@gmail.com>
2016-05-26 21:38:59 -0400

    Assignment 22 update

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,18 +8,18 @@ class AddressBook
     @entries = []
   end
 
-def add_entry(name, phone_number, email)
+  def add_entry(name, phone_number, email)
 
-  index = 0
-  entries.each do |entry|
+    index = 0
+    entries.each do |entry|
 
-    if name < entry.name
-      break
+      if name < entry.name
+        break
+      end
+      index += 1
     end
-    index += 1
-  end
 
-  entries.insert(index, Entry.new(name, phone_number, email))
+    entries.insert(index, Entry.new(name, phone_number, email))
   end
 
   def import_from_csv(file_name)
@@ -54,15 +54,19 @@ def add_entry(name, phone_number, email)
 
     return nil
   end
-end
+
 
   def iterative_search(name)
+
     AddressBook.entries.each do |entry|
+
       if entry.name == name
         return name
       else
         return nil
       end
+
     end
   end
+
 end
